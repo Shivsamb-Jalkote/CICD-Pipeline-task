1751c4678529a70ce1b813e1d5555c712f8d2a36
Shivsamb-Jalkote <166595376+Shivsamb-Jalkote@users.noreply.github.com>
2025-07-17 15:38:51 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,8 +2,6 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
-  sensitive = true
+output "update_kubeconfig_command" {
+  value = "aws eks update-kubeconfig --region us-east-1 --name ${module.eks.cluster_name}"
 }
-
