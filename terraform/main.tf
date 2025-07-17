111028534b11fe4499aebcdff265bc2053f7142e
Shivsamb-Jalkote <166595376+Shivsamb-Jalkote@users.noreply.github.com>
2025-07-17 15:42:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,9 +45,13 @@ module "eks" {
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
+
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
+
+  depends_on = [module.eks]
 }
 
